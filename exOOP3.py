c3cb3928dcfd63352365c278b0304cb7169832af
hs4qwc <hs4qwc@gmail.com>
2022-01-16 22:01:53 +0700

    Update exOOP3.py

--- exOOP3.py
+++ exOOP3.py
@@ -10,11 +10,21 @@ class Person:
 class Person2:
 	# constructor แบบ 2 กำหนดค่า None และใส่ค่าเริ่มต้นที่ต้องการ
 	def __init__(self, fname=None, lname=None, contry='Thailand'):
-		self.fname = '' # ค่าเริ่มต้น ว่างๆ
-		self.lname = ''
-		self.contry = 'Thailand' # ค่าเริ่มต้น
+		self.fname = fname
+		self.lname = lname
+		self.contry = contry
+
+	#Overrides method object (โอเวอร์ไลเมตตอท)
+	def __str__(self):
+		return 'ชื่อ: {} นามสกุล: {} ประเทศ: {}'.format(self.fname, self.lname, self.contry)
+#-----------
+
 
 if __name__ == '__main__':
-	p1 = Person()
-	print(p1.fname)
-	print(p1.contry)
+	# p1 = Person()
+	# print(p1.fname)
+	# print(p1.contry)
+
+	p2 = Person2('ศักดิ์ดา','โชคดีมาก')
+	print(p2)
+
